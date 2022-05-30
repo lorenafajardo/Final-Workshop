3b7f65be892d712098f3ef9672358136f750dd78
Lorena Fajardo Díaz <87463011+lorenafajardo@users.noreply.github.com>
2022-05-30 04:44:32 -0500

    Actualización UserRepository
    Se complementó la documenación

--- backend/usersBingo/src/main/java/com/usersBingo/repository/UserRepository.java
+++ backend/usersBingo/src/main/java/com/usersBingo/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.usersBingo.repository;
 
+/**Imporación de librerias */
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.usersBingo.model.UserModel;
 
+/**
+ * la Interface ContactRepository, se extiende JpaRepository para obtener operaciones CRUD básicas 
+ * 
+ * @author LORENA FAJARDO
+ * @version 1.0
+ */
 public interface UserRepository extends JpaRepository <UserModel, Integer>{ 
 
 }
