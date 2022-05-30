c60b706c2c280cf1192e45c94e938087b20465e5
Lorena Fajardo Díaz <87463011+lorenafajardo@users.noreply.github.com>
2022-05-30 03:03:16 -0500

    Complementación de la documetación
    Se complementó la documentación

--- users/backend/services/userService.js
+++ users/backend/services/userService.js
@@ -1,20 +1,21 @@
+/**
+ * Descripcion: userService.js, Contiene la funcion para enviar los datos a springboot por medio de axios
+ * Auhor: Lorena Fajardo Diaz
+ * Version: 1.0.0
+ */
 'use srict'
 
-const person_register = async( email, password ) =>{
-    
-    await axios({
-        method: 'post',
-        url : 'http://localhost:9090/user',
-        responseType : 'json',
-        data: {
-            'email' : email,
-            'password': password,
-        }
-    }).then (function(response) {
-        if(response.ok) {
-            return response.text()
-        } else {
-            throw "Error en la llamada Ajax";
-        }
-    })
-}
+const data = new FormData();
+   data.append('email', 'ee@gmail.com');
+   data.append('password', '23456');
+   fetch('http://localhost:8080/user', {
+   method: 'POST',
+   body: data
+   }).then(function(response) {
+   if(response.ok) {
+       return response.text()
+   } else {
+       throw "Error en la llamada Ajax";
+   }
+})
+   
