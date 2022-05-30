92e468c2981bd0215da6755c0a581a1f9ee45e95
Lorena Fajardo Díaz <87463011+lorenafajardo@users.noreply.github.com>
2022-05-30 04:37:14 -0500

    Actualización UserModel.js
    Se modificó algunos campos

--- backend/usersBingo/src/main/java/com/usersBingo/model/UserModel.java
+++ backend/usersBingo/src/main/java/com/usersBingo/model/UserModel.java
@@ -1,10 +1,20 @@
+/** Importacio de librerias*/
 package com.usersBingo.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+/**
+ * La clase userModel contiene la definicion de variables a utilizar en la aplicacion y sus respectivos metodos
+ * getters and setters para acceder a ellas
+ * 
+ * @author LORENA FAJARDO
+ * @version 1.0
+ */
+@Entity
 public class UserModel {
 
 	@Id
@@ -16,7 +26,15 @@ public class UserModel {
 
 	@NotBlank(message = "Campo obligatorio")
 	private String password;
+	
+	private String name;
+	
+	private String identification;
+	
+	private String id_game;
 
+	
+	/** Genereacion de Getters y Setters para cada atributo*/
 	public Integer getId() {
 		return id;
 	}
@@ -41,10 +59,47 @@ public class UserModel {
 		this.password = password;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIdentification() {
+		return identification;
+	}
+
+	public void setIdentification(String identification) {
+		this.identification = identification;
+	}
+
+	public String getId_game() {
+		return id_game;
+	}
+
+	public void setId_game(String id_game) {
+		this.id_game = id_game;
+	}
+
+	/** Definicion de constructor vacio*/
 	public UserModel() {
 		super();
 	}
-	
-	
-	
+
+	/** Definicion de constructor con todos los atributos*/
+	public UserModel(Integer id,
+			@NotBlank(message = "Campo obligatorio") String email,
+			@NotBlank(message = "Campo obligatorio") String password,
+			String name, String identification, String id_game) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.identification = identification;
+		this.id_game = id_game;
+	}
+		
 }
