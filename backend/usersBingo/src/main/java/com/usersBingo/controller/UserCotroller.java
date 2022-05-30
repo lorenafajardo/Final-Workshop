3e2e854d5c78ce674d4f6461b9e73f6bd13d9eac
Lorena Fajardo Díaz <87463011+lorenafajardo@users.noreply.github.com>
2022-05-30 04:41:57 -0500

    Acualizacion UserCotroller.js
    Se modificó documentación

--- backend/usersBingo/src/main/java/com/usersBingo/controller/UserCotroller.java
+++ backend/usersBingo/src/main/java/com/usersBingo/controller/UserCotroller.java
@@ -2,21 +2,40 @@ package com.usersBingo.controller;
 
 import java.util.regex.Pattern;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+/**Imporacion de librerias*/
 import com.usersBingo.model.UserModel;
 import com.usersBingo.repository.UserRepository;
 import com.usersBingo.util.Response;
 
+/**
+ * @Description La clase UserCotroller contiene request que trae los datos de nodejs
+ * @author LORENA FAJARDO
+ * @version 1.0
+ */
+
+@Slf4j
+@RestController
+@CrossOrigin(origins ="http://localhost:8080", methods= {RequestMethod.GET,RequestMethod.POST})
+
 public class UserCotroller {
 	
-	@Autowired
+	@Autowired(required=true)
 	private UserRepository userRepository;
     private Response response = new Response();
 	
+    
 	@PostMapping("/user")
     public ResponseEntity<Response> crear(UserModel user) {
         response.data = user;
@@ -26,7 +45,7 @@ public class UserCotroller {
         } catch (Exception exc) {
             response.status = exc.getCause().toString();
             response.error = true;
-            if (Pattern.compile("(usuario.tipo_documento_and_documento_UNIQUE)").matcher(exc.getMessage()).find()) {
+            if (Pattern.compile("(user.email)").matcher(exc.getMessage()).find()) {
                 response.message = "documento duplicado";
                 return new ResponseEntity<Response>(response, HttpStatus.CONFLICT);
             } else {
