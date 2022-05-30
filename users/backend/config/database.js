7b11624ae4a5e44c52605031cf896521b64dbfe7
Lorena Fajardo Díaz <87463011+lorenafajardo@users.noreply.github.com>
2022-05-30 02:57:20 -0500

    Complementación de la documentación
    Se complementa la documetación del proyecto

--- users/backend/config/database.js
+++ users/backend/config/database.js
@@ -1,3 +1,9 @@
+/**
+ * Descripcion: database.js, esta clase contiene la URL de la base de daos de MongoDB
+ * Author: Lorena Fajardo Díaz
+ * Version: 1.0.0
+ */
+
 module.exports = {
 	'url': 'mongodb+srv://user_bingo:user_bingo@bingocluster.s33ifhp.mongodb.net/?retryWrites=true&w=majority'
 };
